1fa1be3c607669e0d980af71feea89944e059fe6
Tola Rom <tolarom27@gmail.com>
2024-12-24 00:27:44 +0700

    added goodbye

--- project_i3.java
+++ project_i3.java
@@ -5,14 +5,16 @@ public class project_i3 {
         Scanner sc = new Scanner(System.in);
         System.out.println("hello hello hello");
         System.out.println("it is working as i planned");
-        System.out.println("Enter your name: ");
+        System.out.print("Enter your name: ");
         String name = sc.nextLine();
         System.out.println("Hello " + name);
-        System.out.println("Enter your age: ");
+        System.out.print("Enter your age: ");
         int age = sc.nextInt();
         System.out.println("You are " + age + " years old");
+        System.out.println("Thank you for using my program");
+        System.out.println("Goodbye");
         sc.close();
-        
+
         
     }
 }
