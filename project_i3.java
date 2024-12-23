7baffccfb6ddfeabb22474895fb99b772deba408
Tola Rom <tolarom27@gmail.com>
2024-12-24 00:20:05 +0700

    added

--- project_i3.java
+++ project_i3.java
@@ -1,6 +1,7 @@
 public class project_i3 {
 
     public static void main(String[] args) {
-        System.out.println(" hello hello hello");
+        System.out.println("hello hello hello");
+        System.out.println("it is working as i planned");
     }
 }
