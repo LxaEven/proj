7698f22e22f7e61ed3c60d5e6e162eb6316887d1
Tola Rom <tolarom27@gmail.com>
2024-12-24 00:24:16 +0700

    new feature

--- project_i3.java
+++ project_i3.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 public class project_i3 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("hello hello hello");
         System.out.println("it is working as i planned");
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+        System.out.println("You are " + age + " years old");
+        sc.close();
+        
+        
     }
 }
