88544abc76f6ae6bc4f8ad586b0b394b80150fcb
LxaEven <tolarom27@gmail.com>
2024-12-24 13:56:02 +0700

    for student

--- project_i3.java
+++ project_i3.java
@@ -9,6 +9,7 @@ public class project_i3 {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Student management system");
         frame.setSize(300, 200);
+        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
         frame.add(panel);
